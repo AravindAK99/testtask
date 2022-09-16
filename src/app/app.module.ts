08d558456c8fe1b53afec8f29a6c3c512fb3e343
AravindAK99 <aravindchl99@gmail.com>
2022-09-16 20:33:37 +0530

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './header and footer/nav/nav.component';
+import { HomeComponent } from './contents/home/home.component';
+import { FooterComponent } from './contents/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
